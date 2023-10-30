1c7db7b84ad73f31f89d61b9d0613d275a0d7141
Michal Moudrý <moudry.michal@live.com>
2023-10-30 21:58:28 +0100

    Fix

--- src/internal/service/user_service.go
+++ src/internal/service/user_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"user-service/internal/service/model/ioc"
 
@@ -26,6 +27,7 @@ func NewUserService(dapr dapr.Client, fbAuth *auth.Client) UserService {
 // Method for deleting a user in the system.
 func (srvc UserService) DeleteUser(ctx context.Context, userId string) error {
 	log.Printf("Try to delete user with ID: %v\n", userId)
+	fmt.Printf("Try to delete user with ID: %v\n", userId)
 	err := srvc.FirebaseAuth.DeleteUser(ctx, userId)
 	if err != nil {
 		return err
